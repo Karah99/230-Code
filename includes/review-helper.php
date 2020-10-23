6b4307394bf583b55bc99df62aa59c4fc7177357
Karah99 <kgl0007@mix.wvu.edu>
2020-10-23 17:20:10 -0400

    Fixed typo in review-helper
    Changed item-id to item_id, also added status to the reviews SQL table. Susan Heffley's account was created as well.

--- includes/review-helper.php
+++ includes/review-helper.php
@@ -8,10 +8,10 @@ if (isset($_POST['review-submit'])) {
     $title = $_POST['review-title'];
     $date = date('Y-m-d H:i:s');
     $review = $_POST['review'];
-    $item_id = $_POST['item-id'];
+    $item_id = $_POST['item_id'];
     $rating = $_POST['rating'];
 
-    $sql = "INSERT INTO reviews (item_id, uname, title, review_text, rev_date, rating_num) VALUES ('$item_id', '$uname', '$title', '$review', '$date', '$rating');";
+    $sql = "INSERT INTO reviews (item_id, uname, title, review_text, rev_date, rating_num, status) VALUES ('$item_id', '$uname', '$title', '$review', '$date', '$rating', 1);";
     $stmt = mysqli_stmt_init($conn);
     $result = $conn->query($sql);
     header("Location: ../review.php?id=$item_id");
